4bafb13042373f1b86b89f3e5f869b6e45b8b915
Paul R Staab <staab@bio.lmu.de>
2014-10-16 11:24:33 +0200

    update script to copy only .cc and .h file

--- tools/update-source.sh
+++ tools/update-source.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #
-# %title%
-# %description%
-# 
-# Author:   Paul R. Staab 
-# Email:    staab (at) bio.lmu.de
-# Date:     2014-09-15
-# Licence:  GPLv3 or later
-#
+# Updates the source code files of scrm in scrm-r/src/scrm
+# with files from scrm's git repo supposed to be at
+# ../scrm
 
-rsync -rvcA --exclude='.*' --exclude='*.o' ../scrm/src/* src/scrm/
+rsync -rvcA --exclude '.*'\
+            --include '*/'\
+            --include '*.cc' --include '*.h'\
+            --exclude='*'\
+            ../scrm/src/* src/scrm/
