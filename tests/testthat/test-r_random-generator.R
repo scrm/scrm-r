37dcb09aed16884c8a4d42b1f34f27886d830292
Paul Staab <develop@paulstaab.de>
2015-03-30 13:57:59 +0200

    silence lint

--- tests/testthat/test-r_random-generator.R
+++ tests/testthat/test-r_random-generator.R
@@ -3,7 +3,7 @@ context("R Random Generator")
 test_that("RRG sample works", {
   set.seed(100)
   for (i in 1:100) {
-    smpl <- test_RRG_sample()
+    smpl <- test_RRG_sample() #nolint
     expect_true(is.numeric(smpl) & is.finite(smpl))
     expect_that(smpl, is_less_than(1))
     expect_that(smpl, is_more_than(0))
@@ -22,7 +22,7 @@ test_that("RRG sampleUnitExpo works", {
 test_that("RRG sampleExpoExpoLimit works", {
   set.seed(100)
   for (i in 1:100) {
-    smpl <- test_RRG_sampleExpoExpoLimit(1, 0, 2)
+    smpl <- test_RRG_sampleExpoExpoLimit(1, 0, 2) #nolint
     expect_true(is.numeric(smpl) & is.finite(smpl))
     expect_true(smpl > 0 | smpl == -1)
     expect_that(smpl, is_less_than(2))
