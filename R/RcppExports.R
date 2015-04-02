fb815e6afec14d747b27acc963c40924de5c3fce
Paul Staab <develop@paulstaab.de>
2015-04-02 17:53:15 +0200

    throw seed warning only necessary

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,25 +2,25 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #' Simulate the evolution of biological sequences
-#' 
+#'
 #' This function provides an interface for calling \emph{scrm} from R.
 #' The command line options are passed via the \code{args} argument.
 #' The vignette 'scrm-Arguments' contains details about the available options.
 #' Summary statistics are converted into an R format. Additionally, there
-#' is an option to write the original command line output into a file. 
-#' 
+#' is an option to write the original command line output into a file.
+#'
 #' @section Seeding:
-#' The R version of scrm uses random number from R's random generator. 
-#' Therefore, the '-seed' argument of the command line version will be ignored, 
-#' and no seed is given in the output. 
-#' Use the R function \code{\link{set.seed}} prior to calling this 
+#' The R version of scrm uses random number from R's random generator.
+#' Therefore, the '-seed' argument of the command line version will be ignored,
+#' and no seed is given in the output.
+#' Use the R function \code{\link{set.seed}} prior to calling this
 #' function to ensure reproduciblity of results.
-#' 
+#'
 #' @param args A string containing the command line arguments for scrm. Look at
 #'             scrms vignette for a description of available arguments.
-#' @param file If provided, scrm will additionally write it is output into 
+#' @param file If provided, scrm will additionally write it is output into
 #'             a file with the given file, using an ms-like text output.
-#' @return A named list of summary statistics. Most summary statistics are 
+#' @return A named list of summary statistics. Most summary statistics are
 #'         again a list, where each entry contains the value for one locus.
 #'         For the site frequency spectrum, the summary statistic is a matrix,
 #'         where each row contains the spectrum for one locus.
@@ -29,7 +29,7 @@
 #' @seealso
 #' \itemize{
 #'   \item \code{vignette('scrm-Arguments')} for an overview of commandline arguments and
-#'   \item \code{vignette('scrm-TreesForApe')} for an example on using 
+#'   \item \code{vignette('scrm-TreesForApe')} for an example on using
 #'         genealogies simulated with \emph{scrm} with package 'ape'.
 #' }
 #' @examples
@@ -37,8 +37,8 @@
 #' sum_stats <- scrm('5 1 -r 5 100 -t 5 -T -L')
 #' str(sum_stats)
 #'
-#' # Simulate the site frequency spectrum at 3 loci. For each locus 
-#' # 10 Chromosomes of 1Mb length are sampled from two populations with 
+#' # Simulate the site frequency spectrum at 3 loci. For each locus
+#' # 10 Chromosomes of 1Mb length are sampled from two populations with
 #' # migration inbetween.
 #' scrm('10 3 -r 400 1000000 -l 100000 -I 2 4 6 0.5 -t 300 -oSFS')$sfs
 #'
--- src/scrmr.cpp
+++ src/scrmr.cpp
@@ -19,25 +19,25 @@ bool write_file;
 // [[Rcpp::plugins(cpp11)]]
 
 //' Simulate the evolution of biological sequences
-//' 
+//'
 //' This function provides an interface for calling \emph{scrm} from R.
 //' The command line options are passed via the \code{args} argument.
 //' The vignette 'scrm-Arguments' contains details about the available options.
 //' Summary statistics are converted into an R format. Additionally, there
-//' is an option to write the original command line output into a file. 
-//' 
+//' is an option to write the original command line output into a file.
+//'
 //' @section Seeding:
-//' The R version of scrm uses random number from R's random generator. 
-//' Therefore, the '-seed' argument of the command line version will be ignored, 
-//' and no seed is given in the output. 
-//' Use the R function \code{\link{set.seed}} prior to calling this 
+//' The R version of scrm uses random number from R's random generator.
+//' Therefore, the '-seed' argument of the command line version will be ignored,
+//' and no seed is given in the output.
+//' Use the R function \code{\link{set.seed}} prior to calling this
 //' function to ensure reproduciblity of results.
-//' 
+//'
 //' @param args A string containing the command line arguments for scrm. Look at
 //'             scrms vignette for a description of available arguments.
-//' @param file If provided, scrm will additionally write it is output into 
+//' @param file If provided, scrm will additionally write it is output into
 //'             a file with the given file, using an ms-like text output.
-//' @return A named list of summary statistics. Most summary statistics are 
+//' @return A named list of summary statistics. Most summary statistics are
 //'         again a list, where each entry contains the value for one locus.
 //'         For the site frequency spectrum, the summary statistic is a matrix,
 //'         where each row contains the spectrum for one locus.
@@ -46,7 +46,7 @@ bool write_file;
 //' @seealso
 //' \itemize{
 //'   \item \code{vignette('scrm-Arguments')} for an overview of commandline arguments and
-//'   \item \code{vignette('scrm-TreesForApe')} for an example on using 
+//'   \item \code{vignette('scrm-TreesForApe')} for an example on using
 //'         genealogies simulated with \emph{scrm} with package 'ape'.
 //' }
 //' @examples
@@ -54,8 +54,8 @@ bool write_file;
 //' sum_stats <- scrm('5 1 -r 5 100 -t 5 -T -L')
 //' str(sum_stats)
 //'
-//' # Simulate the site frequency spectrum at 3 loci. For each locus 
-//' # 10 Chromosomes of 1Mb length are sampled from two populations with 
+//' # Simulate the site frequency spectrum at 3 loci. For each locus
+//' # 10 Chromosomes of 1Mb length are sampled from two populations with
 //' # migration inbetween.
 //' scrm('10 3 -r 400 1000000 -l 100000 -I 2 4 6 0.5 -t 300 -oSFS')$sfs
 //'
@@ -66,15 +66,15 @@ List scrm(std::string args, std::string file = "") {
   Param param("scrm " + args);
   Model model;
   param.parse(model);
-  
+
   // Print help or version information if user asked for it
   if (param.help()) stop("Please use '?scrm' for help");
   if (param.version()) {
     return List::create(_("version") = VERSION);
   }
-  
+
   RRandomGenerator rrg;
-  
+
   /** Open a file for writing if 'file' is given */
   if (file.length() > 0) {
     fs.open(file);
@@ -85,40 +85,40 @@ List scrm(std::string args, std::string file = "") {
   } else {
     write_file = false;
   }
-  
+
   /** Throw a warning if -seed argmuent is used */
-  if (param.random_seed() != -1) 
-    Rf_warning("Ignoring Seed argument. Set a seed in R.");
-  
+  if (param.seed_is_set())
+    Rf_warning("Ignoring seed argument. Set a seed in R.");
+
   /** Throw a warning if no summary statistics are used */
   if (model.countSummaryStatistics() == 0)
     Rf_warning("No summary statisics specified. No output will be produced.");
-  
+
   Forest forest = Forest(&model, &rrg);
   List sum_stats = initSumStats(forest);
 
   // Loop over the independent loci/chromosomes
   for (size_t rep_i=0; rep_i < model.loci_number(); ++rep_i) {
-    
+
     // Mark the start of a new independent sample
     if (write_file) fs << std::endl << "//" << std::endl;
-    
+
     // Now set up the ARG, and sample the initial tree
     forest.buildInitialTree();
-    
-    while (forest.next_base() < model.loci_length()) { 
+
+    while (forest.next_base() < model.loci_length()) {
       forest.sampleNextGenealogy();
     }
-    
+
     addLocusSumStats(forest, rep_i, sum_stats);
     if (write_file) forest.printLocusSumStats(fs);
-    
+
     forest.clear();
   }
-  
+
   /** Clean up */
   if (write_file) fs.close();
   rrg.clearFastFunc();
-  
+
   return sum_stats;
 }
